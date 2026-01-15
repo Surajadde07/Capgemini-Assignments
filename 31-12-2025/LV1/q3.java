3b4ca6aee08c88eddfd7890feb15ff856feb091c
suraj <sharadadde@gmail.com>
2026-01-15 22:10:24 +0530

    first commit

--- 31-12-2025/LV1/q3.java
+++
@@ -1,8 +0,0 @@
-
-public class q3 {
-    public static void main(String[] args) {
-        double distance = 10.8;
-        double miles = distance / 1.6;
-        System.out.println("The distance of " + distance + " kilometers is: " + miles + " miles.");
-    }
-}
